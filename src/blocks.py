45d2d9ae7bd86450b38fa3623ed4d12d3f0f76b9
Caleb Gardner <calebq23@gmail.com>
2024-03-11 19:53:51 -0500

    More stuff

--- src/blocks.py
+++ src/blocks.py
@@ -8,6 +8,43 @@ valid_blocks = [
     "ordered_list"
 ]
 
-def markdown_to_blocks(text):
-    return text.split("\n\n")
+def block_to_block_type(block: str):
+    if block.startswith("# "):
+        return valid_blocks[1]
+    if block.startswith("```") and block.endswith("```"):
+        return valid_blocks[2]
+    if block.startswith(">"):
+        return valid_blocks[3]
+    if block.startswith("*") or block.startswith("-"):
+        newlines = block.count("\n")
+        bullets = block.count("\n*") + block.count("\n-")
+        if newlines == bullets:
+            return valid_blocks[4]
+    if block.startswith("1."):
+        num = 1
+        valid = True
+        for l in block.split("\n"):
+            if not l.startswith(str(num)+"."):
+                valid = False
+                break
+        if valid:
+            return valid_blocks[5]
+    return valid_blocks[0] 
 
+def markdown_to_blocks(text):
+    code_block_spl = text.split("```")
+    if len(code_block_spl) == 1:
+        return text.split("\n\n")
+    if len(code_block_spl)%2 == 0:
+        raise Exception("uneven code blocks (```). Invalid markdown")
+    middle = False
+    out = []
+    for s in code_block_spl:
+        if not middle:
+            middle = True
+            if s == "":
+                out += s.split("\n\n")
+            continue
+        middle = False
+        out.append("```"+s+"```")
+    return out
